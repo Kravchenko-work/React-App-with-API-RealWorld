ace95879f489a7f9362872d36dd651f6e0d45289
Slava <kravchienko_slava@mail.ru>
2022-06-22 00:34:45 +0400

    Добавление в репозиторий текущего состояния - 25 урок

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom";
+
+import Routes from 'routes';
+import TopBar from 'components/TopBar';
+import { CurrentUserProvider } from 'contexts/currentUser';
+import CurrentUserChecker from 'components/CurrentUserChecker';
+
+const App = () => {
+    return (
+        <CurrentUserProvider>
+            <CurrentUserChecker>
+                <Router>
+                    <TopBar></TopBar>
+                    <Routes></Routes>
+                </Router>
+            </CurrentUserChecker>
+        </CurrentUserProvider>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
